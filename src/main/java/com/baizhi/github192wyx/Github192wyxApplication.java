605ca7e3f3e282f6a03716c0f7f7058bad5a5d2b
wangyx <3163223987@qq.com>
2020-07-15 22:22:11 +0800

    first commit

---
+++ src/main/java/com/baizhi/github192wyx/Github192wyxApplication.java
@@ -0,0 +1,15 @@
+package com.baizhi.github192wyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Github192wyxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Github192wyxApplication.class, args);
+    }
+//rm: cannot remove '.git/': Is a directory
+    //fatal: pathspec '.git/' did not match any files
+    //find: ‘./$RECYCLE.BIN/S-1-5-18’: Permission denied
+}
---
+++ src/test/java/com/baizhi/github192wyx/Github192wyxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.baizhi.github192wyx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Github192wyxApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
